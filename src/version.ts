5bdebae0f955e5b52732d8f8169791766a58f977
Rok Černezel <rok@kubus.site>
2026-05-25 19:30:31 +0200

    feat: update build number to 260525.02 in version files

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 export const KUBUS_VERSION = '0.6.0'
-export const KUBUS_BUILD_NUMBER = '260525.01'
-export const KUBUS_BUILD_NUMBER_NUMERIC = 26052501
+export const KUBUS_BUILD_NUMBER = '260525.02'
+export const KUBUS_BUILD_NUMBER_NUMERIC = 26052502
 export const KUBUS_BUILD_DATE = '2026-05-25'
 export const KUBUS_CHANNEL = 'alpha'
