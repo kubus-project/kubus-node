b1c7dc23c94802617cc40c67905a313375e9e9dc
Rok Černezel <rok@kubus.site>
2026-06-15 11:33:36 +0200

    feat: update version to 0.6.2 and adjust build number and date in related files

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
-export const KUBUS_VERSION = '0.6.1'
-export const KUBUS_BUILD_NUMBER = '260609.01'
-export const KUBUS_BUILD_NUMBER_NUMERIC = 26060901
-export const KUBUS_BUILD_DATE = '2026-06-09'
+export const KUBUS_VERSION = '0.6.2'
+export const KUBUS_BUILD_NUMBER = '260615.01'
+export const KUBUS_BUILD_NUMBER_NUMERIC = 26061501
+export const KUBUS_BUILD_DATE = '2026-06-15'
 export const KUBUS_CHANNEL = 'alpha'
